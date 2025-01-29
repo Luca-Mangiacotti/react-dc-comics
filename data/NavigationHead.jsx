bd77c8b4145c67f39d3f8e342556aa719c0ff792
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-01-29 15:57:25 +0100

    feat: add data file of navigatione bar of Header

---
+++ data/NavigationHead.jsx
@@ -0,0 +1,64 @@
+const NavigationHead = [
+  {
+    id: 1,
+    url: "#",
+    title: "characters",
+    current: false,
+  },
+  {
+    id: 2,
+    url: "#",
+    title: "comics",
+    current: true,
+  },
+  {
+    id: 3,
+    url: "#",
+    title: "movies",
+    current: false,
+  },
+  {
+    id: 4,
+    url: "#",
+    title: "tv",
+    current: false,
+  },
+  {
+    id: 5,
+    url: "#",
+    title: "games",
+    current: false,
+  },
+  {
+    id: 6,
+    url: "#",
+    title: "collectibles",
+    current: false,
+  },
+  {
+    id: 7,
+    url: "#",
+    title: "videos",
+    current: false,
+  },
+  {
+    id: 8,
+    url: "#",
+    title: "fans",
+    current: false,
+  },
+  {
+    id: 9,
+    url: "#",
+    title: "news",
+    current: false,
+  },
+  {
+    id: 10,
+    url: "#",
+    title: "shop",
+    current: false,
+  },
+];
+
+export default NavigationHead;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,66 +1,4 @@
-const navigationHead = [
-  {
-    id: 1,
-    url: "#",
-    title: "characters",
-    current: false,
-  },
-  {
-    id: 2,
-    url: "#",
-    title: "comics",
-    current: true,
-  },
-  {
-    id: 3,
-    url: "#",
-    title: "movies",
-    current: false,
-  },
-  {
-    id: 4,
-    url: "#",
-    title: "tv",
-    current: false,
-  },
-  {
-    id: 5,
-    url: "#",
-    title: "games",
-    current: false,
-  },
-  {
-    id: 6,
-    url: "#",
-    title: "collectibles",
-    current: false,
-  },
-  {
-    id: 7,
-    url: "#",
-    title: "videos",
-    current: false,
-  },
-  {
-    id: 8,
-    url: "#",
-    title: "fans",
-    current: false,
-  },
-  {
-    id: 9,
-    url: "#",
-    title: "news",
-    current: false,
-  },
-  {
-    id: 10,
-    url: "#",
-    title: "shop",
-    current: false,
-  },
-];
-
+import NavigationHead from "../../data/NavigationHead";
 export default function Header() {
   return (
     <header>
@@ -68,10 +6,9 @@ export default function Header() {
         <img src="..\img\dc-logo.png" alt="" />
         <nav>
           <ul className="row">
-            {navigationHead.map((link) => {
+            {NavigationHead.map((link) => {
               return (
                 <li key={"link.id"}>
-                  {" "}
                   <a href="{link.url}">{link.title}</a>
                 </li>
               );
